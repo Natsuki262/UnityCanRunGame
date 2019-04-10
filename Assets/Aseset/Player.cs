ed6febaea9e94c2f877a6f884c5684698e59e891
NatsukiFGL <hiro.58.fgl.2525.952@icloud.com>
2019-04-11 00:30:25 +0900

    【add】Unityちゃんを走らせることは成功した。 がモーションの出が違和感ある

---
+++ Assets/Aseset/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Running();
+    }
+    void Running()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * 1.0f;
+            animator.SetBool("is_running", true);
+            Debug.Log("プシュッ");
+        }
+        else
+        {
+            animator.SetBool("is_running", false);
+        }
+        
+    }
+}
